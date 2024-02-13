75169b03f7e58b587672c119b56bad01c3a022ae
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-02-13 09:54:50 +0100

    Complete the app pages and components.

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,7 +1,15 @@
+import MeetupDetail from '@/components/meetups/MeetupDetail';
+import { Fragment } from 'react';
+
 const MeetupDetails = () => {
   return (
-    <div>MeetupDetails</div>
-  )
-}
+    <MeetupDetail
+      image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg'
+      title='A First Meetup'
+      address='Some address 5, 12345 Some City'
+      description='This is a first meetup!'
+    />
+  );
+};
 
 export default MeetupDetails;
